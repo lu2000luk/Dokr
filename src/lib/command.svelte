bb1f19422ecea5fbf2e8417486c45200e7c40614
lu2000luk <lu2000luk@gmail.com>
2024-11-29 19:34:03 +0100

    Fixed UI bug

--- src/lib/command.svelte
+++ src/lib/command.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex p-2 my-2 rounded bg-[#0f1013] overflow-x-auto" style="{currentlyEditing ? "border: 2px solid #10b981;" : ""}">
-    <span class="text-[#7e22ce] cursor-pointer hover:bg-text hover:bg-opacity-80 hover:text-background p-1 pr-2 rounded command_section command_command">{command}</span>
+    <span class="text-[#7e22ce] cursor-pointer hover:bg-text hover:bg-opacity-80 hover:text-background p-1 mr-1 rounded command_section command_command">{command}</span>
     {#each args as arg}
         {#if arg === ":" || arg === "="}
             <span class="text-[#14b8a6] cursor-pointer py-1">{arg}</span>
